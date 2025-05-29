080d83e6c1f1e757603ec2bfc46ac27a59fc52e6
FTTroy <troianofrancesco01@gmail.com>
2025-05-29 19:19:58 +0200

    fix log time ping

--- src/main/java/com/github/fttroy/reminder/web/controller/PingController.java
+++ src/main/java/com/github/fttroy/reminder/web/controller/PingController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Slf4j
 @RestController
 @RequestMapping("/ping")
 public class PingController {
+    private static final String ROME_ZONE_ID = "Europe/Rome";
 
     @GetMapping("/")
     public int ping() {
-        log.info("pinged at {}", LocalDateTime.now());
+        log.info("pinged at {}", ZonedDateTime.now(ZoneId.of(ROME_ZONE_ID)).toLocalDateTime());
         return 1;
     }
 }
